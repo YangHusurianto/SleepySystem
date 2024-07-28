d6e245d7e4f08e28aa5586818fc0b7a43eea00ba
Yang H <yang.husurianto@gmail.com>
2024-07-27 17:52:30 -0700

    feat: context menu delete command

--- src/commands/tools/delete.ts
+++ src/commands/tools/delete.ts
@@ -1,130 +1,62 @@
-import { ApplicationCommandType } from "discord.js";
-import { MessageContextMenuCommand } from "../../structures/Command.ts";
-import { ExtendedInteraction } from "../../typings/Command.ts";
+import { MessageContextMenuCommand } from '../../structures/Command.ts';
+import { ExtendedInteraction } from '../../typings/Command.ts';
+import { logAction } from '../../utils/logs.ts';
+import { ExtendedClient } from '../../structures/Client.ts';
+import { botSelfCheck, roleHeirarchyCheck } from '../../utils/checks.ts';
+
+
+import { ApplicationCommandType, escapeMarkdown, User } from 'discord.js';
 
 export default new MessageContextMenuCommand({
-  name: "Delete Message",
+  name: 'Delete Message',
   type: ApplicationCommandType.Message,
-  run: async ({ interaction } : { interaction: ExtendedInteraction }) => {
-    console.log("test");
+  run: async ({ interaction, client }: { interaction: ExtendedInteraction, client: ExtendedClient }) => {
     if (!interaction.isMessageContextMenuCommand()) return;
 
-    console.log(interaction);
+    const { guild, member } = interaction;
     const message = interaction.targetMessage;
+    const target = message.author;
 
-    console.log(message);
+    if (await botSelfCheck(interaction, target, client, 'delete a message of')) return;
+    if (await roleHeirarchyCheck(interaction, guild, target, member, 'delete a message of')) return;
 
-    await interaction.reply({
-      content: `Message (${message.content}) deleted.`,
-      ephemeral: true,
-    })
+    deleteMessage(interaction, message, guild, target, member);
   },
 });
 
+async function deleteMessage(
+  interaction: ExtendedInteraction,
+  message: any,
+  guild: any,
+  target: any,
+  member: any
+) {
+  await message
+    .delete()
+    .then(async () => {
+      await interaction.reply({
+        content: `Message (${message.content}) deleted.`,
+        ephemeral: true,
+      });
 
-// import { logAction } from '../../utils/logs.js';
-// import { botSelfCheck, roleHeirarchyCheck } from '../../utils/checks.js';
-
-// import { SlashCommandBuilder, escapeMarkdown } from 'discord.js';
-
-// export default {
-//   data: new SlashCommandBuilder()
-//     .setName('delete')
-//     .setDescription('Delete a message')
-//     .addStringOption((option) =>
-//       option
-//         .setName('id')
-//         .setDescription('The id of the message to delete')
-//         .setRequired(true)
-//     )
-//     .setDMPermission(false),
-
-//   async execute(interaction, client) {
-//     const { options, guild, member } = interaction;
-//     const messageId = options.getString('id');
-
-//     let message;
-//     try {
-//       message = await interaction.channel.messages.fetch(messageId);
-//     } catch (err) {
-//       return await interaction.reply({
-//         content: 'Failed to fetch message or invalid message id.',
-//         ephemeral: true,
-//       });
-//     }
-
-//     if (!message) {
-//       return await interaction.reply({
-//         content: 'Message not found.',
-//         ephemeral: true,
-//       });
-//     }
-
-//     const target = message.author;
-
-//     if (await botSelfCheck(interaction, target, client, 'delete a message of'))
-//       return;
-//     if (
-//       await roleHeirarchyCheck(
-//         interaction,
-//         guild,
-//         target,
-//         member,
-//         'delete a message of'
-//       )
-//     )
-//       return;
-
-//     if (await channelCheck(interaction, message)) return;
-
-//     try {
-//       deleteMessage(interaction, message, guild, target, member);
-//     } catch (err) {
-//       console.error(err);
-//     }
-//   },
-// };
-
-// const deleteMessage = async (interaction, message, guild, target, member) => {
-//   await message
-//     .delete()
-//     .then(async () => {
-//       await interaction.reply({
-//         content: `Message (${message.content}) deleted.`,
-//         ephemeral: true,
-//       });
-
-//       await logAction(
-//         guild,
-//         `**DEL MSG**\n` +
-//           `**Author:** ${escapeMarkdown(`${target.username} (${target.id}`, {
-//             code: true,
-//           })})\n` +
-//           `**Content:** ${message.content}\n` +
-//           `**Moderator:** ${escapeMarkdown(
-//             `${member.user.username} (${member.user.id}`,
-//             { code: true }
-//           )})\n`
-//       );
-//     })
-//     .catch(async (err) => {
-//       console.error(err);
-//       return await interaction.reply({
-//         content: 'Failed to delete message.',
-//         ephemeral: true,
-//       });
-//     });
-// };
-
-// const channelCheck = async (interaction, message) => {
-//   let channelName = message.channel.name;
-
-//   if (channelName === 'mod-actions' || channelName === 'logs') {
-//     return await interaction.reply({
-//       content: 'You cannot delete messages in this channel.',
-//       ephemeral: true,
-//     });
-//   }
-
-//   return false;
-// };
+      await logAction(
+        guild,
+        `**DEL MSG**\n` +
+          `**Author:** ${escapeMarkdown(`${target.username} (${target.id}`, {
+            inlineCode: true,
+          })})\n` +
+          `**Content:** ${message.content}\n` +
+          `**Moderator:** ${escapeMarkdown(
+            `${member.user.username} (${member.user.id}`,
+            { inlineCode: true }
+          )})\n`
+      );
+    })
+    .catch(async (err: any) => {
+      console.error(err);
+      return await interaction.reply({
+        content: 'Failed to delete message.',
+        ephemeral: true,
+      });
+    });
+}
