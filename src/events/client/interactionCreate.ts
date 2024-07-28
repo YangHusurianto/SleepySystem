61e5ae4eb58bbecad68c0b68ee9449dc3ee98d23
Yang H <yang.husurianto@gmail.com>
2024-07-27 17:52:46 -0700

    fix: add context menu interaction handling

--- src/events/client/interactionCreate.ts
+++ src/events/client/interactionCreate.ts
@@ -1,22 +1,22 @@
-import { CommandInteractionOptionResolver } from "discord.js";
-import { client } from "../../index.ts";
-import { Event } from "../../structures/Event.ts";
-import { ExtendedInteraction } from "../../typings/Command.ts";
+import { CommandInteractionOptionResolver } from 'discord.js';
+import { client } from '../../index.ts';
+import { Event } from '../../structures/Event.ts';
+import { ExtendedInteraction } from '../../typings/Command.ts';
 
-export default new Event("interactionCreate", async (interaction) => {
-    // Chat Input Commands
-    if (interaction.isChatInputCommand()) {
-        const command = client.commands.get(interaction.commandName);
+export default new Event('interactionCreate', async (interaction) => {
+  // Chat Input Commands
+  if (interaction.isChatInputCommand() || interaction.isContextMenuCommand()) {
+    const command = client.commands.get(interaction.commandName);
 
-        if (!command) {
-          console.error(`Command ${interaction.commandName} not found`);
-          return;
-        }
-
-        command.run({
-            args: interaction.options as CommandInteractionOptionResolver,
-            client,
-            interaction: interaction as ExtendedInteraction
-        });
+    if (!command) {
+      console.error(`Command ${interaction.commandName} not found`);
+      return;
     }
+
+    command.run({
+      args: interaction.options as CommandInteractionOptionResolver,
+      client,
+      interaction: interaction as ExtendedInteraction,
+    });
+  }
 });
